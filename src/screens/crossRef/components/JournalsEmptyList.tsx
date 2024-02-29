a3c2a6d282ca8947b1fdd62df3ca255f4381c56b
abdiel49 <abdielorellana3@gmail.com>
2024-02-29 09:45:08 -0400

    update empty list component

--- src/screens/crossRef/components/JournalsEmptyList.tsx
+++ src/screens/crossRef/components/JournalsEmptyList.tsx
@@ -1,14 +1,19 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
+import TextComponent from '../../../components/atoms/TextComponent';
 
 const JournalsEmptyList = () => {
   return (
-    <View>
-      <Text>JournalsEmptyList</Text>
+    <View style={styles.container}>
+      <TextComponent text={'Not Data Here'} H2 bold />
     </View>
   );
 };
 
 export default JournalsEmptyList;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+});
